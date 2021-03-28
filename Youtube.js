9b2600da9db9662e064a5aad164bb01c987bf5dc
MaliBYK <developerbox00@gmail.com>
2021-03-28 21:07:12 +0300

    deleted all and started to create  from scratch

--- Youtube.js
+++ Youtube.js
@@ -1,16 +1,1 @@
-class Youtube {
-  getSubscribers(channelKey, apiKey) {
-    return new Promise((resolve, reject) => {
-      fetch(
-        `https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${channelKey}&key=${apiKey}`
-      )
-        .then(response => response.json())
-        .then(data => {
-          document.querySelector(".subscribers-td").innerHTML =
-            data["items"][0].statistics.subscriberCount;
-          resolve(data);
-        })
-        .catch(err => reject(err));
-    });
-  }
-}
+`https://www.googleapis.com/youtube/v3/channels?part=statistics&id=${this.channelKey}&key=${this.apiKey}`;
--- app.js
+++ app.js
@@ -1,20 +1,3 @@
 const apiKey = "AIzaSyAoig1xivyTjsgD5lfNYt3ppQ_AKgdIZVQ";
 const input = document.querySelector(".link-input");
 const searchBtn = document.querySelector(".search");
-
-let channelKey;
-const youtube = new Youtube();
-
-searchBtn.addEventListener("click", search);
-
-function search(e) {
-  value = input.value;
-  channelKey = value.split("/channel/");
-  console.log(channelKey);
-  if (channelKey.length > 1) {
-    channelKey = channelKey[1];
-    youtube.getSubscribers(channelKey, apiKey).then(data => console.log(data));
-  } else {
-    console.log("HATA");
-  }
-}
