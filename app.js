dac98704200fab4eabf57de6cb2a2c1b234860ad
MaliBYK <developerbox00@gmail.com>
2021-03-29 21:52:09 +0300

    did nthing

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
 const apiKey = "AIzaSyAoig1xivyTjsgD5lfNYt3ppQ_AKgdIZVQ";
 const input = document.querySelector(".link-input");
 const searchBtn = document.querySelector(".search");
+const channelKey;
+
+searchBtn.addEventListener("click",function(){
+  if (input.value.split("/channel/").length > 1){
+    channelKey = input.value.split("/channel/").split[1];
+  }
+  else console.log("Hata");
+})
